6d6e591880856374ec1d1c365da4abd152228a02
prasanth <sgrandhisiri@islander.tamucc.edu>
2022-08-23 14:01:48 -0500

    pyscript add 10000 rows completed

---
+++ html/py/table.py
@@ -0,0 +1,23 @@
+
+import js
+from js import document
+import time
+from js import console
+
+
+def addrowspy(*args):
+    start = time.time()
+    tableRef = document.getElementById('myTable').getElementsByTagName('tbody')[0]
+    for i in range(10000):
+        newRow = tableRef.insertRow(tableRef.rows.length)
+        idCell = newRow.insertCell(0)
+        idCell.appendChild(document.createTextNode(i + 1))
+        nameCell = newRow.insertCell(1)
+        name = document.createTextNode('Person  ' + str(i))
+        nameCell.appendChild(name)
+        ageCell = newRow.insertCell(2)
+        age = document.createTextNode(20)
+        ageCell.appendChild(age)
+    end = time.time()
+    console.log(end-start)
+
