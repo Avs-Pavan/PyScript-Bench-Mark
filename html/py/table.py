d06c1ca84cad5e6511c258a3d67bcf2720aef895
prasanth <sgrandhisiri@islander.tamucc.edu>
2022-08-23 23:40:56 -0500

    clear all rows issue fixed

--- html/py/table.py
+++ html/py/table.py
@@ -21,3 +21,41 @@ def addrowspy(*args):
     end = time.time()
     console.log(end-start)
 
+def clearTable(*args):
+    start = time.time()
+    myTable = document.getElementById('myTable')
+    rowCount = myTable.rows.length
+    i = 1
+    while i < (rowCount):
+        try:
+            myTable.deleteRow(i)
+            i =+ 1
+        except:
+            break
+    end = time.time()
+    console.log(end-start)
+
+def increaseAge(*args):
+    start = time.time()
+    myTable = document.getElementById('myTable')
+    i=0
+    for i in range(10000):
+        cells = myTable.rows[i].cells
+        cells[2].innerHTML = str(21)
+    end = time.time()
+    console.log(end-start)
+
+def clearEvenRows(*args):
+    start = time.time()
+    myTable = document.getElementById('myTable')
+    rowCount = myTable.rows.length
+    i = 1
+    while i < (rowCount):
+        try:
+            if i % 2 == 0:
+                myTable.deleteRow(i)
+            i =+ 1
+        except:
+            break
+    end = time.time()
+    console.log(end-start)
