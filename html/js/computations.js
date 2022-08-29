607d3bc28df552b15bc9990c9ebec8f416161ef5
prasanth <sgrandhisiri@islander.tamucc.edu>
2022-08-29 09:39:08 -0500

    pyscript merge sort added

--- html/js/computations.js
+++ html/js/computations.js
@@ -5,32 +5,33 @@ function doSort() {
         .then(data => {
             console.log("Table creation started")
             const start = performance.now()
-            mergeSort(data)
+            var arr_size = data.length;
+            mergeSort(data, 0, arr_size - 1)
             const duration = performance.now() - start
             console.log("Time taken to merge sort  1M numbers")
             colorLog(duration, "success")
         })
 }
-function mergeSort(array) {
-    const half = array.length / 2
-    if (array.length < 2) {
-        return array
-    }
-    const left = array.splice(0, half)
-    return merge(mergeSort(left), mergeSort(array))
-}
+// function mergeSort(array) {
+//     const half = array.length / 2
+//     if (array.length < 2) {
+//         return array
+//     }
+//     const left = array.splice(0, half)
+//     return merge(mergeSort(left), mergeSort(array))
+// }
 
-function merge(left, right) {
-    let arr = []
-    while (left.length && right.length) {
-        if (left[0] < right[0]) {
-            arr.push(left.shift())
-        } else {
-            arr.push(right.shift())
-        }
-    }
-    return [...arr, ...left, ...right]
-}
+// function merge(left, right) {
+//     let arr = []
+//     while (left.length && right.length) {
+//         if (left[0] < right[0]) {
+//             arr.push(left.shift())
+//         } else {
+//             arr.push(right.shift())
+//         }
+//     }
+//     return [...arr, ...left, ...right]
+// }
 
 function multiplyMatrices(m1, m2) {
     var result = [];
@@ -238,4 +239,73 @@ class PolynomialHash {
 
         return charCode;
     }
+}
+
+
+
+function merge(arr, l, m, r) {
+    var n1 = m - l + 1;
+    var n2 = r - m;
+
+    // Create temp arrays
+    var L = new Array(n1);
+    var R = new Array(n2);
+
+    // Copy data to temp arrays L[] and R[]
+    for (var i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (var j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+    // Merge the temp arrays back into arr[l..r]
+
+    // Initial index of first subarray
+    var i = 0;
+
+    // Initial index of second subarray
+    var j = 0;
+
+    // Initial index of merged subarray
+    var k = l;
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    // Copy the remaining elements of
+    // L[], if there are any
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    // Copy the remaining elements of
+    // R[], if there are any
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+// l is for left index and r is
+// right index of the sub-array
+// of arr to be sorted */
+function mergeSort(arr, l, r) {
+    if (l >= r) {
+        return;//returns recursively
+    }
+    var m = l + parseInt((r - l) / 2);
+    mergeSort(arr, l, m);
+    mergeSort(arr, m + 1, r);
+    merge(arr, l, m, r);
 }
