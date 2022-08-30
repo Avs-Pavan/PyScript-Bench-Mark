317406ad6a3d58a0050f761a7415e7ddbac142e3
prasanth <sgrandhisiri@islander.tamucc.edu>
2022-08-30 13:59:44 -0500

    heap sort added

--- html/js/computations.js
+++ html/js/computations.js
@@ -108,44 +108,94 @@ function heapSortData() {
         })
 
 }
-var array_length;
-/* to create MAX  array */
-function heap_root(input, i) {
-    var left = 2 * i + 1;
-    var right = 2 * i + 2;
-    var max = i;
 
-    if (left < array_length && input[left] > input[max]) {
-        max = left;
+function heapSort( arr)
+    {
+        var N = arr.length;
+ 
+        // Build heap (rearrange array)
+        for (var i = Math.floor(N / 2) - 1; i >= 0; i--)
+            heapify(arr, N, i);
+ 
+        // One by one extract an element from heap
+        for (var i = N - 1; i > 0; i--) {
+            // Move current root to end
+            var temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+ 
+            // call max heapify on the reduced heap
+            heapify(arr, i, 0);
+        }
     }
+ 
+    // To heapify a subtree rooted with node i which is
+    // an index in arr[]. n is size of heap
+    function heapify(arr, N, i)
+    {
+        var largest = i; // Initialize largest as root
+        var l = 2 * i + 1; // left = 2*i + 1
+        var r = 2 * i + 2; // right = 2*i + 2
+ 
+        // If left child is larger than root
+        if (l < N && arr[l] > arr[largest])
+            largest = l;
+ 
+        // If right child is larger than largest so far
+        if (r < N && arr[r] > arr[largest])
+            largest = r;
+ 
+        // If largest is not root
+        if (largest != i) {
+            var swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+ 
+            // Recursively heapify the affected sub-tree
+            heapify(arr, N, largest);
+        }
+    }
+ 
+   
 
-    if (right < array_length && input[right] > input[max]) {
-        max = right;
-    }
+// var array_length;
+// /* to create MAX  array */
+// function heap_root(input, i) {
+//     var left = 2 * i + 1;
+//     var right = 2 * i + 2;
+//     var max = i;
 
-    if (max != i) {
-        swap(input, i, max);
-        heap_root(input, max);
-    }
-}
+//     if (left < array_length && input[left] > input[max]) {
+//         max = left;
+//     }
 
-function swap(input, index_A, index_B) {
-    var temp = input[index_A];
-    input[index_A] = input[index_B];
-    input[index_B] = temp;
-}
+//     if (right < array_length && input[right] > input[max]) {
+//         max = right;
+//     }
 
-function heapSort(input) {
-    array_length = input.length;
-    for (var i = Math.floor(array_length / 2); i >= 0; i -= 1) {
-        heap_root(input, i);
-    }
-    for (i = input.length - 1; i > 0; i--) {
-        swap(input, 0, i);
-        array_length--;
-        heap_root(input, 0);
-    }
-}
+//     if (max != i) {
+//         swap(input, i, max);
+//         heap_root(input, max);
+//     }
+// }
+
+// function swap(input, index_A, index_B) {
+//     var temp = input[index_A];
+//     input[index_A] = input[index_B];
+//     input[index_B] = temp;
+// }
+
+// function heapSort(input) {
+//     array_length = input.length;
+//     for (var i = Math.floor(array_length / 2); i >= 0; i -= 1) {
+//         heap_root(input, i);
+//     }
+//     for (i = input.length - 1; i > 0; i--) {
+//         swap(input, 0, i);
+//         array_length--;
+//         heap_root(input, 0);
+//     }
+// }
 
 
 const p1 = 31;
